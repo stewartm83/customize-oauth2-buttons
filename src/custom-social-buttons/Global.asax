9a3f272439b934ac46034d95be5bf04d1a43c4f6
Stewart <stewart.mbofana@live.com>
2016-11-17 14:42:40 +0200

    Initial Code commit

---
+++ src/custom-social-buttons/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="custom_social_buttons.MvcApplication" Language="C#" %>
---
+++ src/custom-social-buttons/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(custom_social_buttons.Startup))]
+namespace custom_social_buttons
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
